a7e8d87071effc072b6feefbd12b4fdb1a5e8411
Feipe Flores <felipe@avantdigital.com.br>
2022-11-07 10:25:40 -0300

    Adicionados comentários sobre o funcionamento

--- scan.php
+++ scan.php
@@ -3,14 +3,26 @@
 require './conexao.php';
 
 
-$iterator = null;
+$iterator = NULL;
 
 print_r(
     $conexao->scan(
         $iterator,
         [
-            'match' => 'CHAVE-*',
-            'count' => 50,
+            'match' => 'NOTIFICATIONREP72*',
+            'count' => 20000,
         ]
     )
 );
+
+// SCAN 0 COUNT 20000 MATCH NOTIFICATIONREP72*
+
+
+/**
+ * match = PALAVRA A SER BUSCADA, CASO QUEIRA BUSCAR POR UMA PALAVRA PARCIAL USAR O *
+ * count = TAMANHO DO BLOCO A SER ANALISADO PARA VER SE É LOCALIZADA A PALAVRA DESEJADA, A BUSCA VAI SER REALIZADA NOS PRIMEIOS 20K DE REGISTROS 
+ * 
+ * OBS-1: o retorno é aletório
+ * OBS-2: a palavra será localizada dentro do range informado no count ou seja: a palavra 'NOTIFICATIONREP72' será localizada dentro dos primeiros 20k de registros.
+ * OBS-3: $iterator == paginação ??
+ */
